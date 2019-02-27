b0e92e567dbf295b07836202b92b5c65270801bd
nakat-t <armaiti.wizard@gmail.com>
2019-02-27 11:53:43 +0900

    Add Q.15

---
+++ q15/src/main.rs
@@ -0,0 +1,24 @@
+use clap::*;
+use std::io::BufRead;
+
+fn main() {
+    let app = app_from_crate!()
+        .arg(Arg::from_usage("[NUM] 'print the first NUM lines'").default_value("10"));
+    let matches = app.get_matches();
+
+    let num = matches.value_of("NUM").unwrap();
+    let num = match num.parse::<usize>() {
+        Ok(n) => n,
+        Err(_) => {
+            println!("error: NUM must be an integer");
+            std::process::exit(1);
+        }
+    };
+
+    let stdin = std::io::stdin();
+    let handle = stdin.lock();
+    let lines: Vec<String> = handle.lines().map(|l| l.unwrap()).collect();
+    for line in lines.iter().skip(lines.len() - num) {
+        println!("{}", line);
+    }
+}

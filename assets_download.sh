67bbd03cea07ccecbdc17d46a67b8f13832834dd
nakat-t <armaiti.wizard@gmail.com>
2019-03-06 13:30:18 +0900

    update assets_download.sh

--- assets_download.sh
+++ assets_download.sh
@@ -2,3 +2,5 @@
 
 mkdir -p assets
 wget -O assets/hightemp.txt http://www.cl.ecei.tohoku.ac.jp/nlp100/data/hightemp.txt
+wget -O assets/jawiki-country.json.gz http://www.cl.ecei.tohoku.ac.jp/nlp100/data/jawiki-country.json.gz
+gunzip assets/jawiki-country.json.gz

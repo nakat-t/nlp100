5f397259c3d99100c176fc6e058d8ac4300892a9
nakat-t <armaiti.wizard@gmail.com>
2019-03-07 22:25:34 +0900

    Add Q.24

---
+++ q24/src/main.rs
@@ -0,0 +1,28 @@
+use regex::Regex;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::io::BufRead;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct JaWikiCountry {
+    text: String,
+    title: String,
+}
+
+fn main() {
+    let re = Regex::new(r"\[\[(?:ファイル|File):([^|]+)\|").unwrap();
+    let stdin = std::io::stdin();
+    let handle = stdin.lock();
+    let mut map = HashMap::new();
+    for l in handle.lines() {
+        let entry: JaWikiCountry = serde_json::from_str(&l.unwrap()).expect("Invalid JSON format");
+        map.entry(entry.title.clone()).or_insert(entry);
+    }
+    if let Some(country) = map.get("イギリス") {
+        for line in country.text.lines() {
+            for cap in re.captures_iter(line) {
+                println!("{}", &cap[1]);
+            }
+        }
+    }
+}

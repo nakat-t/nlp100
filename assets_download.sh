ba3173aac5a0cc875c0ef4d5c7655c616daa0950
nakat-t <armaiti.wizard@gmail.com>
2019-03-16 17:16:13 +0900

    Ready for Q.30

--- assets_download.sh
+++ assets_download.sh
@@ -4,3 +4,5 @@ mkdir -p assets
 wget -O assets/hightemp.txt http://www.cl.ecei.tohoku.ac.jp/nlp100/data/hightemp.txt
 wget -O assets/jawiki-country.json.gz http://www.cl.ecei.tohoku.ac.jp/nlp100/data/jawiki-country.json.gz
 gunzip assets/jawiki-country.json.gz
+wget -O assets/neko.txt http://www.cl.ecei.tohoku.ac.jp/nlp100/data/neko.txt
+mecab < assets/neko.txt > assets/neko.txt.mecab

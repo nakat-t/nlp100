90505a1e220d2c48df7c30f709d9285b5c6b9211
nakat-t <armaiti.wizard@gmail.com>
2019-03-31 22:35:37 +0900

    Update asset_download.sh

--- assets_download.sh
+++ assets_download.sh
@@ -6,3 +6,4 @@ wget -O assets/jawiki-country.json.gz http://www.cl.ecei.tohoku.ac.jp/nlp100/dat
 gunzip assets/jawiki-country.json.gz
 wget -O assets/neko.txt http://www.cl.ecei.tohoku.ac.jp/nlp100/data/neko.txt
 mecab < assets/neko.txt > assets/neko.txt.mecab
+cabocha -f 1 < assets/neko.txt > assets/neko.txt.cabocha
